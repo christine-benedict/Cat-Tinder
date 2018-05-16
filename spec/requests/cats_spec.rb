8403b825b7c801b3aefd6d07bf0378a8d6ab58e1
Christine Benedict <learn@learn08.localdomain>
2018-05-16 10:58:44 -0700

    added create and rspec

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -17,4 +17,27 @@ describe "Cats API" do
     # Assure that we got one result back as expected
     expect(json.length).to eq 1
   end
+
+  it "creates a cat" do
+    # The params we are going to send with the request
+    cat_params = {
+      cat: {
+        name: 'Buster',
+        age: 4,
+        enjoys: 'Meow Mix, and plenty of sunshine.'
+      }
+    }
+
+    # Send the request to the server
+    post '/cats', params: cat_params
+
+    # Assure that we get a success back
+    expect(response).to be_success
+
+    # Look up the cat we expect to be created in the Database
+    new_cat = Cat.first
+
+    # Assure that the created cat has the correct attributes
+    expect(new_cat.name).to eq('Buster')
+  end
 end
