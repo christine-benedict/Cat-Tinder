2b9b5f2ee13ded2885dbf76baa50314fe7064e23
Christine Benedict <learn@learn08.localdomain>
2018-05-16 11:08:50 -0700

    create cat validation added

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,18 +5,20 @@ class CatsController < ApplicationController
   end
 
   def create
-    # Create a new cat
     cat = Cat.create(cat_params)
-
-    # respond with our new cat
+    if cat.valid?
     render json: cat
+   else
+     render json: cat.errors, status: :unprocessable_entity
+   end
   end
 
   private
-  
+
   # Handle strong parameters, so we are secure
   def cat_params
     params.require(:cat).permit(:name, :age, :enjoys)
   end
+
 
 end
